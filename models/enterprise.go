454cecaaf0f15194298dfd546e4b3033d94e16f0
NekruzRakhimov <nekruzusa111@gmail.com>
2023-07-31 10:36:41 +0500

    add route for getting enterprises

--- models/enterprise.go
+++ models/enterprise.go
@@ -1,12 +1,15 @@
 package models
 
 type Enterprise struct {
-	Id          int     `json:"id"`
-	Title       string  `json:"title"`
-	Price       float64 `json:"price"`
-	Description string  `json:"description"`
-	Category    string  `json:"category"`
-	Image       string  `json:"image"`
+	ID                int     `json:"id"`
+	Name              string  `json:"name"`
+	OwnershipType     string  `json:"ownership_type"`
+	Okved             string  `json:"okved"`
+	INN               string  `json:"inn"`
+	EIN               string  `json:"ein"`
+	RegistrationDate  string  `json:"registration_date"`
+	Address           string  `json:"address"`
+	AuthorizedCapital float64 `json:"authorized_capital"`
 	//Rating      struct {
 	//	Rate  float64 `json:"rate"`
 	//	Count int     `json:"count"`
